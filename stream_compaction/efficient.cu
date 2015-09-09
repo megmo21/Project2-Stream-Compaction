58540474acc19315d47eb2c061d5f6cd8861a899
unknown <momeg@MOR100C-04.base.seas.upenn.edu>
2015-09-09 15:25:43 -0400

    part 3 in progress:

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -6,14 +6,58 @@
 namespace StreamCompaction {
 namespace Efficient {
 
-// TODO: __global__
+int* g_odata;
+int* g_idata;
 
+__global__ void kern_up_sweep(int n, int *odata, const int *idata, int layer) {
+	int thrId = layer*threadIdx.x;
+	odata[thrId + layer - 1] += idata[thrId + (layer / 2) - 1];
+}
+
+__global__ void kern_down_sweep(int n, int *odata, const int *idata, int layer) {
+	int thrId = n - 1 - layer*threadIdx.x;
+	int temp = idata[thrId + (layer / 2) - 1];
+	odata[thrId + (layer / 2) - 1] = idata[thrId + layer - 1];
+	odata[thrId + layer - 1] = temp + idata[thrId + layer - 1];
+}
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+	cudaMalloc((void**)&g_odata, n * sizeof(int));
+	cudaMalloc((void**)&g_idata, n * sizeof(int));
+
+	int* scanArray = new int[n];
+	scanArray[0] = 0;
+	for (int i = 1; i < n; i++) {
+		scanArray[i] = idata[i - 1];
+	}
+
+	cudaMemcpy(g_odata, odata, n*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(g_idata, scanArray, n*sizeof(int), cudaMemcpyHostToDevice);
+	
+    for (int d = 0; d <= ilog2ceil(n) - 1; d++) {
+		int layer = pow(2, d + 1);
+		float mult = 1.0f / (float)layer;
+		g_odata = g_idata;
+		int block = ceil(n*mult);
+		kern_up_sweep<<<1, block>>>(n, g_odata, g_idata, layer);
+		g_idata = g_odata;
+	}
+	cudaMemcpy(scanArray, g_idata, n*sizeof(int), cudaMemcpyDeviceToHost);
+	scanArray[n-1] = 0;
+	cudaMemcpy(g_idata, scanArray, n*sizeof(int), cudaMemcpyDeviceToHost);
+	for (int d = ilog2ceil(n) - 1; d >= 0; d--) {
+		int layer = pow(2, d + 1);
+		float mult = 1.0f / (float)layer;
+		g_odata = g_idata;
+		int block = ceil(n*mult);
+		kern_down_sweep<<<1, block>>>(n, g_odata, g_idata, layer);
+		g_idata = g_odata;
+	}
+
+	cudaMemcpy(odata, g_odata, n*sizeof(int), cudaMemcpyDeviceToHost);
+
 }
 
 /**
