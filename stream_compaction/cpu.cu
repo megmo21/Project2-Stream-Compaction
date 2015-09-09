cd6196012bf73b95e232a2323bc3be597bb8817b
unknown <momeg@MOR100B-03.base.seas.upenn.edu>
2015-09-09 11:59:55 -0400

    part 1 passed

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -9,7 +9,11 @@ namespace CPU {
  */
 void scan(int n, int *odata, const int *idata) {
     // TODO
-    printf("TODO\n");
+	odata[0] = 0;
+	for (int i = 1; i < n; i++) {
+		odata[i] = idata[i-1] + odata[i-1];
+	}
+    
 }
 
 /**
@@ -18,8 +22,17 @@ void scan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithoutScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+    int sum = 0;
+	for (int i = 0; i < n; i++) {
+		if (idata[i] == 0) {
+			odata[i] = 0;
+		}
+		else {
+			odata[i] = 1;
+			sum++;
+		}
+	}
+    return sum;
 }
 
 /**
@@ -28,8 +41,18 @@ int compactWithoutScan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+    for (int i = 0; i < n; i++) {
+		if (idata[i] == 0) {
+			odata[i] = 0;
+		}
+		else {
+			odata[i] = 1;
+		}
+	}
+	int* scanArray = new int[n];
+	scan(n, scanArray, odata);
+
+    return scanArray[n - 1] + odata[n - 1];
 }
 
 }
